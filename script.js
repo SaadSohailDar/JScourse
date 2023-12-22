bae3ab1e5641d63f2b7e6f8027e2cc7d515a4393
Saad <saad8398@gmail.com>
2023-12-22 18:47:10 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,107 @@
+/*let js = 'amazing';
+console.log(40+8+23-10);
+console.log("Jonas");
+
+let firstName="Jonas";
+
+
+
+true;
+let javascriptIsFun=true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+
+console.log(typeof javascriptIsFun);
+
+console.log(typeof 23);
+
+console.log(typeof 'saad');
+
+javascriptIsFun="YES!";
+console.log(typeof javascriptIsFun);
+
+let age=30;
+age=31;
+
+const birthYear=1991;
+birthYear=1990;
+
+var job='programmer';
+job='teacher';
+
+
+const now=2037;
+const ageSaad = now - 1991;
+const ageArsalan = now - 2018;
+console.log(ageSaad,ageArsalan,2**3);
+
+
+const marksWeight=78;
+const marksHeight=1.69;
+marksBMI=marksWeight/(marksHeight**2);
+
+const JohnWeight=92;
+const JohnHeight= 1.95;
+const JohnBMI=JohnWeight/(JohnHeight**2);
+
+var markHigherBMI=false;
+if(marksBMI>JohnBMI)
+{
+    markHigherBMI=true;
+}
+else{
+    markHigherBMI=false;
+}
+
+console.log(markHigherBMI);
+
+
+
+const inputYear='1991';
+console.log(Number(inputYear),inputYear);
+console.log(Number(inputYear)+18);
+
+
+const age=18;
+
+if(age=== 18) console.log('you just became adult')
+
+const Dolphins1=97;
+const Dolphins2=112;
+const Dolphins3=101;
+const Koalas1=109;
+const Koalas2=95;
+const Koalas3=123;
+const dolphinsAvg=Dolphins1+Dolphins2+Dolphins3/3;
+const koalaAvg=Koalas1+Koalas2+Koalas3/3;
+
+if(dolphinsAvg>koalaAvg)
+{
+    console.log("Dolphins are winner");
+}
+else
+{
+    console.log("Koalas are winner");
+}
+
+const day= 'monday';
+
+switch(day)
+{
+    case 'monday':
+        console.log("day is monday");
+        console.log("day is monday");
+        break;
+    case 'tuesday':
+        console.
+}   */
+
+
+const age=11;
+
+age>=18 ? console.log('i like to drink wine'):console.log('i like to drink Water');
+
+const drink= age>=18 ?'Wine':'drink';
+
+console.log(drink);
