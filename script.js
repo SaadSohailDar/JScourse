2c095dc875674fabf0295d02a059ad7f6a8cfaa4
Saad <saad8398@gmail.com>
2023-12-27 13:21:36 +0500

    Spread Operator on objects and arrays

--- script.js
+++ script.js
@@ -32,8 +32,23 @@ const restaurant = {
     console.log(obj);
   },
 
-  
+  orderPasta:function(ing1,ing2,ing3)
+  {
+    console.log(`here is your pasta with ${ing1},${ing2},${ing3}`);
+  },
 };
+
+
+
+const ingredients=[
+  prompt(`Lets\'s make Pasta! Ingredient 1?`),
+  prompt(`Lets\'s make Pasta! Ingredient 2?`),
+  prompt(`Lets\'s make Pasta! Ingredient 3?`)
+];
+
+console.log(ingredients);
+
+restaurant.orderPasta(...ingredients);
 
 restaurant.orderDelivery({
   time:'22:30',
@@ -41,6 +56,10 @@ restaurant.orderDelivery({
   mainIndex: 2,
   starterIndex: 2 ,
 })
+
+//objects
+const newRestaurant={FoundedIn:1998 ,...restaurant,Founder:'saad',}
+console.log(newRestaurant);
 
 const {name,openingHours,categories}=restaurant;
 console.log(name,openingHours,categories);
@@ -67,6 +86,38 @@ const {
   fri:{open:o,close:c},
 } = openingHours;
 console.log(o,c);
+
+
+const arr=[7,8,9];
+
+const badNewArr=[1,2,arr[0],arr[1],arr[2]];
+console.log(badNewArr);
+
+//Spread Operator
+const newArr=[1,2, ...arr];
+console.log(newArr);
+
+
+console.log(...newArr);
+
+const newMenu=[...restaurant.mainMenu,'sushi'];
+console.log(newMenu);
+
+//Copy Array
+const mainMenuCopy={...restaurant.mainMenu};
+
+
+//join arrays
+const manu=[...restaurant.mainMenu,...restaurant.starterMenu];
+console.log(manu);
+
+const str= 'Saad';
+const Letters= [...str,' ','S.'];
+console.log(Letters);
+console.log(...str);
+
+
+
 
 // const arr= [2,3,4];
 // const a=arr[0]; 
